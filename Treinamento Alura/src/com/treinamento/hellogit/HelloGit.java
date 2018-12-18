fa006d2c7a7c660c419c1e35f4de57616b80b2a1
Alexander Souza <45544256+Alexander-Souza@users.noreply.github.com>
2018-12-18 18:33:40 -0200

    Adicionei arquivos via upload

---
+++ Treinamento Alura/src/com/treinamento/hellogit/HelloGit.java
@@ -0,0 +1,7 @@
+package com.treinamento.hellogit;
+//Exemplo de integração com o git hub...
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello git!!!");
+	}
+}
