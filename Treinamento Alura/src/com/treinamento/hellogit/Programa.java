2547f3ae807d51abcd00a3dd38c3e0d44de254cb
Alexander Souza <alexanderfs.souza@outlook.com>
2018-12-19 23:40:17 -0200

    Adicionando primeiro programa do treinamento Alura.

---
+++ Treinamento Alura/src/com/treinamento/hellogit/Programa.java
@@ -0,0 +1,8 @@
+package com.treinamento.hellogit;
+
+public class Programa {
+	public static void main(String[] args) {
+		System.out.println("Alexander seja bem vindo a aula da Alura realizado na IDE Eclipse");
+	}
+
+}
