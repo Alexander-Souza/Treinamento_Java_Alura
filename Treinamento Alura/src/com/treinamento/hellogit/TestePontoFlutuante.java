0854367108a544662c2411e2ecef8ec393ad57aa
Alexander Souza <alexanderfs.souza@outlook.com>
2018-12-20 00:20:07 -0200

    Exercicio realizado nas aulas 5 e 6.

---
+++ Treinamento Alura/src/com/treinamento/hellogit/TestePontoFlutuante.java
@@ -0,0 +1,22 @@
+package com.treinamento.hellogit;
+
+public class TestePontoFlutuante {
+	public static void main(String[] args) {
+		 
+		double salario = 1250.79;
+		
+		System.out.println("Meu salario é: "+salario);
+		
+		double divisao = 3.14 / 2;
+		
+		System.out.println("O valor da divisao é: "+divisao);
+		
+		int outraDivisao = 5/2;
+		
+		System.out.println("Nova Divisao: "+outraDivisao);
+		
+		double novaTentativa = 5.0/2;
+		
+		System.out.println("Nova tentativa "+novaTentativa);
+	}
+}
---
+++ Treinamento Alura/src/com/treinamento/hellogit/TesteVariaveis.java
@@ -0,0 +1,19 @@
+package com.treinamento.hellogit;
+
+public class TesteVariaveis {
+	public static void main(String[] args) {
+		
+		int idade = 38;
+		
+		System.out.println("A minha idade é: "+idade);
+		
+		idade = 30 + 10;
+		
+		System.out.println("A idade é: "+idade);
+		
+		idade = (7*5)+2;
+		
+		System.out.println("Nova idade: "+idade);
+	}
+
+}
