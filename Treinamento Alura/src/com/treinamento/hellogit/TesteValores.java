30319fac0ab3f93232c776ab438e4cc412e3ee72
Alexander Souza <alexanderfs.souza@outlook.com>
2018-12-21 23:39:26 -0200

    Exercicios realizado na aula 06

---
+++ Treinamento Alura/src/com/treinamento/hellogit/TesteValores.java
@@ -0,0 +1,21 @@
+package com.treinamento.hellogit;
+
+public class TesteValores {
+	public static void main(String[] args) {
+		int primeiro = 5;
+		int segundo = 7;
+		segundo = primeiro;
+		primeiro = 10;
+		
+		System.out.println(segundo);
+		
+		String saudacao = "Olá, meu nome é ";
+		String nome = "Alexander ";
+		String continuacao = "e minha idade é ";
+		int idade = 100;
+		
+		System.out.println(saudacao+nome+continuacao+idade);
+		
+	}
+
+}
